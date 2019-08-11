81a2b9a8c578dbb17ebb2143e3aac032f2133f92
brody <brody.m.dyck@tutanota.com>
2019-08-11 17:19:11 -0400

    ddclient setup now

--- cfg/ddclient.nix
+++ cfg/ddclient.nix
@@ -11,6 +11,8 @@
       # Use imperative configuration to keep secrets out of the (world
       # readable) Nix store. If this option is not set, the NixOS options from
       # services.ddclient.* will be used to populate /etc/ddclient.conf.
+      #ddclientSecrets="/var/lib/ddclient/secrets"
+      #mkdir /var/lib/ddclient/secrets && cp wherever/ddclient.conf ${ddclientSecrets}/ddclient.conf
       configFile = "/var/lib/ddclient/secrets/ddclient.conf";
     };
   };
